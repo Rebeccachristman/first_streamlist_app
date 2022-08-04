11ceb2f6d5fecd1573834416ca5e2ccbc28ac031
Rebeccachristman <Rebeccachristman@gmail.com>
2022-08-04 11:06:47 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,20 +27,23 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  streamlit.write('The user entered ', fruit_choice)
+  if not fruit_choice:
+      streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    #streamlit.text(fruityvice_response)
+    streamlit.text(fruityvice_response.json())
 
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#streamlit.text(fruityvice_response)
-
-streamlit.text(fruityvice_response.json())
-
-# flatten the json
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# put the flattened json into a pandas dataframe
-streamlit.dataframe(fruityvice_normalized)
-
+    # flatten the json
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # put the flattened json into a pandas dataframe
+    streamlit.dataframe(fruityvice_normalized)
+except URLERROR as e:
+    streamlit.error()
+    
 streamlit.stop()
 
 
